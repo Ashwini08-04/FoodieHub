07b3ceadd00d732451506ffe5c4934f2ed2844bb
Ashwini Sonawane <sonawaneashwini073@gmail.com>
2026-09-08 12:37:46 +0530

    Switch FoodieHub to PostgreSQL

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const path = require("path");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: path.join(__dirname, "../database.sqlite"),
-  logging: false, // Set to console.log to see SQL queries
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = sequelize;
